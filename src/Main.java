0d15141a53da07cb219f254933bb09b8f20dca54
akash kumar <akashkumarsrm16@gmail.com>
2023-02-08 10:00:38 +0530

    UC_3ValidEmailId

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,34 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" This is User Registration Form ");
-        Pattern p = Pattern.compile("[A-Z][a-z]*");
-        System.out.println("Enter your name:");
-        String firstName = sc.next();
+ class FormDetails {
+    Scanner sc = new Scanner(System.in);
+    Pattern p = Pattern.compile("[A-Z][a-z]*");
+    Pattern p1 = Pattern.compile("abc[.][a-z]{3}@bl[.]co[.][a-z]{2}");
+    void lastName(){
 
-        Matcher m = p.matcher(firstName);
+        System.out.println("Enter your last name:");
+        String lastName = sc.next();
+        Matcher m = p.matcher(lastName);
         if (m.find()) {
             System.out.println("Valid Name");
         } else {
             System.out.println("Invalid Name");
         }
-        FormDetails f1 = new FormDetails();
-        f1.lastName();
-        //&& m.group().equals(args[0])
+    }
+    void emailId()
+    {
+        System.out.println("Enter your email Id:");
+        String emailId = sc.next();
+        Matcher m = p1.matcher(emailId);
+        if (m.find()) {
+            System.out.println("Valid Email Id");
+        } else {
+            System.out.println("Invalid Email ID");
+        }
+
 
     }
+
 }
